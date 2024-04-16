74ba50aa72d443b9c3fd874a996ad90cbdcf80f8
滑稽盾采紫由 <1751114377@qq.com>
2024-04-17 02:33:37 +0800

    更改gitignore

--- four.py
+++ four.py
@@ -1,3 +1,113 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:736dc69f573febf328185d8e5cf7ba9f7f90f02a3233e0421aecf26a6884ca7f
-size 375
+class exercise:
+    url = ""
+    answer = '''
+    '''
+
+
+u1_3_1 = exercise()
+u1_3_1.url = r'#/course-v1:Unipus+nhce_3_rw_4_sz+20201231/courseware/u1/u1g13/u1g17/u1g18/p_1'
+u1_3_1.answer ='''
+1) delicacy
+2) bankruptcy
+3) accountancy
+4) secrecy
+5) vacancy
+6) urgency
+7) atmospheric
+8) magnet
+9) metallic
+10) gloom
+11) guilt
+12) mastery '''
+
+u1_3_2 = exercise()
+u1_3_2.url = r'#/course-v1:Unipus+nhce_3_rw_4_sz+20201231/courseware/u1/u1g13/u1g17/u1g19/p_1'
+u1_3_2.answer = '''
+1. bankruptcies
+2. atmospheric
+3. delicacies
+4. urgency
+5. accountancy
+6. gloom
+7. magnet
+8. metallic
+9. mastery
+10. vacancy
+11. guilt
+12. secrecy
+'''
+u1_3_3 = exercise()
+u1_3_3.url = r'#/course-v1:Unipus+nhce_3_rw_4_sz+20201231/courseware/u1/u1g13/u1g20/u1g21/p_1'
+u1_3_3.answer = '''
+1) mentioned
+2) determine
+3) gained
+4) responsible
+5) heavily
+6) artistic
+7) opposite
+8) analytical
+9) distorted
+10) stumped
+'''
+
+u1_3_4 = exercise()
+u1_3_4.url = r'#/course-v1:Unipus+nhce_3_rw_4_sz+20201231/courseware/u1/u1g13/u1g22/u1g23/p_1'
+u1_3_4.answer = '''
+1) were dripping with
+2) in exchange for
+3) flared up
+4) make an analogy between
+5) set a date for
+6) make
+7) out of
+8) made a pact
+9) had appealed to
+'''
+
+u1_8_1 = exercise()
+u1_8_1.url = r'#/course-v1:Unipus+nhce_3_rw_4_sz+20201231/courseware/u1/u1g43/u1g44/u1g45/p_1'
+u1_8_1.answer = '''
+1. triggering
+2. obscure
+3. hypothesis
+4. formulate
+5. threshold
+6. incidence
+7. refute
+8. realm
+9. decay
+10. testimony
+'''
+
+u1_8_2 = exercise()
+u1_8_2.url = r'#/course-v1:Unipus+nhce_3_rw_4_sz+20201231/courseware/u1/u1g43/u1g46/u1g47/p_1'
+u1_8_2.answer = '''
+1) play the odds
+2) subject to
+3) attributes
+4) to
+5) be factored into
+6) call for
+7) By virtue of
+8) get stuck on
+9) come into play
+'''
+u1_9_1 = exercise()
+u1_9_1.url = r'#/course-v1:Unipus+nhce_3_rw_4_sz+20201231/courseware/u1/u1g51/u1g53/u1g55/p_1'
+u1_9_1.answer = '''
+1) available
+2) erroneous
+3) valid
+4) intellectual
+5) lightening
+6) inaccurate
+7) winning
+8) distort
+9) logic-proof
+10) tremendous
+11) watertight
+12) cognitive
+'''
+
+exercises = [u1_8_1, u1_8_2, u1_9_1]
--- unipus.py
+++ unipus.py
@@ -1,3 +1,120 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:daf09b1a3f4fb8c99462b3748031fc501aa0a86b356015a344ffd7ee9a0b9fb1
-size 3517
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import re
+import time
+import config
+import four as unipus
+from selenium.common.exceptions import TimeoutException
+
+
+# 指定chromedriver的路径
+webdriver_service = Service(config.user.chormedriverlocation)
+
+# 指定Chrome的路径
+chrome_options = Options()
+chrome_options.binary_location = config.user.chormelocation
+
+# 创建一个新的Chrome浏览器实例
+driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+
+# 让浏览器打开目标网页
+driver.get(r"https://sso.unipus.cn/sso/login?service=https%3A%2F%2Fu.unipus.cn%2Fuser%2Fcomm%2Flogin")
+# 搜索name="username"的元素，定位到账号输入框
+username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "username")))
+username = driver.find_element(By.NAME, "username")
+# 输入账号
+username.send_keys(config.user.username)
+# 搜索name="password"的元素，定位到密码输入框
+password = driver.find_element(By.NAME, "password")
+# 输入密码
+password.send_keys(config.user.password)
+# 勾选所有<input type="checkbox">
+checkbox = driver.find_elements(By.CSS_SELECTOR, "input[type='checkbox']")
+for i in checkbox:
+    i.click()
+# 搜索id="login"的元素，定位到登录按钮
+login = driver.find_element(By.ID, "login")
+login.click()
+
+input("Press Enter to continue...")
+currenturl = driver.current_url
+#截取url井号之前的部分（包括井号）
+urlprefix = currenturl.split("#")[0]
+
+for i in unipus.exercises:
+    # 用户手动在命令行输入时向下进行
+
+    #url拼接
+    url = urlprefix + i.url
+    print (url)
+    #让浏览器打开目标网页
+    driver.get(url)
+    try:
+        button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[.//span[contains(text(),'确定')]]")))
+        button.click()
+    except TimeoutException:
+        pass
+    #使用正则匹配当前url
+    answer = i.answer
+    # for i in unipus.exercises:
+    #     print (i.url , currenturl)
+    #     print (re.search(i.url, currenturl))
+    #     if re.search(i.url, currenturl):
+    #         answer = i.answer
+    #         break
+    #     else:
+    #         continue
+    # if answer == "":
+    #     print("未找到对应答案")
+    #     continue
+    answer = answer.replace("(", " ")
+    answer = answer.replace(")", " ")
+    answer = answer.replace("（", " ")
+    answer = answer.replace("）", " ")
+    answer= answer.replace(".", " ")
+    answer = re.sub('\d+', ' ', answer)
+    # answer = answer.replace("\r", "").replace("\n", "")
+    #按照空格分割字符串
+    answer = answer.replace("\n\n", "\n")
+    # answer = answer.replace("\n", "")
+    answer = answer.split("\n")
+    answer = [i for i in answer if i]  # 移除空字符串
+    for i in range(len(answer)):
+        print (answer[i])
+    # 等待页面加载完成
+    try:
+        submit = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'提交')]")))
+    except TimeoutException:
+        print("页面加载失败")
+    # 搜索所有<input type="text">
+    inputs = driver.find_elements(By.CSS_SELECTOR, "input[type='text']")
+    for i in range(len(inputs)):
+        inputs[i].send_keys(answer[i])
+        pass
+    
+    #搜索<button>提交</button>
+    input("Press Enter to continue...")
+    submit = driver.find_element(By.XPATH, "//button[contains(text(),'提交')]")
+    submit.click()
+    time.sleep(1)
+    #搜索确认提交的<button>确认</button>
+    # if WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, "//button//div//div//span[contains(text(),'确认')]"))) != None:
+    #     submit_confirm = driver.find_element(By.XPATH, "//button//div//div//span[contains(text(),'确认')]")
+    #     submit_confirm.click()
+    try:
+        submit_confirm = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, "//button//div//div//span[contains(text(),'确认')]")))
+        submit_confirm.click()
+    except TimeoutException:
+        pass
+    
+    # 用户手动在命令行输入时向下进行
+    input("Press Enter to continue...")
+    
+        
+
+
+
